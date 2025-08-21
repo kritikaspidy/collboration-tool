13bf41b1d1e6824a49dc15004d624d0a064e0af4
Krishna Grover <krishnagr1910@gmail.com>
2025-08-22 01:31:08 +0530

    Updated app.js and projectController.js

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,9 @@ const tasksRouter = require('./controllers/task.js');
 require('dotenv').config();
 
 const app = express();
+const projectsRouter = require('./routes/projects');
+
+app.use('/api/projects', projectsRouter);
 
 app.use(cors());
 app.use(express.json());
@@ -23,3 +26,5 @@ app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
